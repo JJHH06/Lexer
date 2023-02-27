abe788094f58772cbfde69a26d1309c8b01cd6fd
José Hurtarte <jjhh121106@gmail.com>
2023-02-26 23:27:41 -0600

    Added naive validation just in case

--- Lab A/lexer.py
+++ Lab A/lexer.py
@@ -74,6 +74,100 @@ def validate_input(user_input):
         print('Invalid input: Second of operation misssing, error at position: {}'.format(operator_closing_parenthesis_regex.start()))
         return False
     
+    
+    
+    parenthesis_count = 0
+    for i in range(len(user_input)):
+        if (user_input[i] == '('):
+            parenthesis_count += 1
+        elif (user_input[i] == ')'):
+            parenthesis_count -= 1
+        if (parenthesis_count < 0):
+            print('Invalid input: Mismatched parenthesis, cannot close a parenthesis that was not opened, error at position: {}'.format(i))
+            return False
+    if (parenthesis_count != 0):
+        print('Invalid input: Parenthesis mismatch error, close all parenthesis to fix it')
+        return False
+    return True
+
+def validate_input(user_input):
+    if (len(user_input) == 0):
+        print ('Invalid input: Empty input')
+        return False
+    if ('.' in user_input):
+        print ('Invalid input: . is a reserved character')
+        return False
+    two_ors_regex = re.search(r'\|\s*\|', user_input)
+    if (two_ors_regex):
+        print('Invalid input: Two or more consecutive |, error at position: {}'.format(two_ors_regex.start()))
+        return False
+    initial_symbol_regex = re.search(r'^\s*[*+?|.]', user_input)
+    if (initial_symbol_regex):
+        print('Invalid input: Symbols at start are not a valid operation, error at position: {}'.format(initial_symbol_regex.start()))
+        return False
+    final_symbol_regex = re.search(r'[\s]*[|.]$', user_input)
+    if (final_symbol_regex):
+        print('Invalid input: Symbol at end not valid, error at position: {}'.format(final_symbol_regex.start()))
+        return False
+    empty_parenthesis_regex = re.search(r'\(\s*\)', user_input)
+    if (empty_parenthesis_regex):
+        print('Invalid input: Empty parenthesis, error at position: {}'.format(empty_parenthesis_regex.start()))
+        return False
+    operator_open_parenthesis_regex = re.search(r'\(\s*[+*?|.]', user_input)
+    if (operator_open_parenthesis_regex):
+        print('Invalid input: Operators after open parenthesis are not a valid operation, error at position: {}'.format(operator_open_parenthesis_regex.start()))
+        return False
+    operator_closing_parenthesis_regex = re.search(r'[|.]\s*\)', user_input)
+    if (operator_closing_parenthesis_regex):
+        print('Invalid input: Second of operation misssing, error at position: {}'.format(operator_closing_parenthesis_regex.start()))
+        return False
+    parenthesis_count = 0
+    for i in range(len(user_input)):
+        if (user_input[i] == '('):
+            parenthesis_count += 1
+        elif (user_input[i] == ')'):
+            parenthesis_count -= 1
+        if (parenthesis_count < 0):
+            print('Invalid input: Mismatched parenthesis, cannot close a parenthesis that was not opened, error at position: {}'.format(i))
+            return False
+    if (parenthesis_count != 0):
+        print('Invalid input: Parenthesis mismatch error, close all parenthesis to fix it')
+        return False
+    return True
+
+def validate_input_naive(user_input):
+    if len(user_input) == 0:
+        print('Invalid input: Empty input')
+        return False
+
+    if '.' in user_input:
+        print('Invalid input: . is a reserved character')
+        return False
+
+    if '||' in user_input:
+        print('Invalid input: Two or more consecutive |')
+        return False
+
+    if user_input.startswith(('*', '+', '?', '|', '.')):
+        print('Invalid input: Symbols at start are not a valid operation')
+        return False
+
+    if user_input.endswith(('|', '.')):
+        print('Invalid input: Symbol at end not valid')
+        return False
+
+    for i in range(len(user_input)):
+        if user_input[i:i+2] == '()':
+            print('Invalid input: Empty parenthesis')
+            return False
+
+        if user_input[i] == '(' and (i+1 == len(user_input) or user_input[i+1] in '+*?|.'):
+            print('Invalid input: Operators after open parenthesis are not a valid operation')
+            return False
+
+        if user_input[i] in '|.' and (i == 0 or user_input[i-1] in '|(' or i+1 == len(user_input) or user_input[i+1] in '|)'):
+            print('Invalid input: {} not valid'.format(user_input[i]))
+            return False
     parenthesis_count = 0
     for i in range(len(user_input)):
         if (user_input[i] == '('):
@@ -95,7 +189,16 @@ def main():
     user_input = sys.argv[1] if len(sys.argv) > 1 else input('Enter a regular expression: ')
     # user_input = '0?(1?)?0*' #Dummy input, uncomment for debugging
 
-    if validate_input(user_input) and (len(user_input) > 0):
+    if len(sys.argv) > 2:
+        if sys.argv[2] == 'naive_validation':
+            is_input_valid = validate_input_naive(clean_input(user_input))
+        else:
+            is_input_valid = validate_input(user_input)
+    else:
+        is_input_valid = validate_input(user_input)
+
+
+    if is_input_valid and (len(user_input) > 0):
         user_input = clean_input(user_input)
         user_input = format_input(user_input)
         output = shunting_yard(user_input)
